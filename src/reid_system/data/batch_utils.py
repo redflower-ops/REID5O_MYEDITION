23651ac6016fa3fbe00dbfc29d813f426ccf4ec6
xhl126719 <937187960@qq.com>
2026-02-18 21:45:31 +0800

    day1

--- src/reid_system/data/batch_utils.py
+++
@@ -1,9 +0,0 @@
-from collections import Counter
-
-
-def count_modalities(modalities: list) -> dict:
-    """
-    输入：modalities，例如 ["vis","vis","nir",...]
-    输出：dict，例如 {"vis": 6, "nir": 2}
-    """
-    return dict(Counter(modalities))
